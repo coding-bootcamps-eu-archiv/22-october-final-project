4ccfc2aad982dd6167cb3a363317083640df0b1d
Mi Mai <m.h.mi@hotmail.de>
2023-02-01 17:38:45 +0100

    add: delete function to button

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import ListComponent from "@/components/ListComponent.vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import ButtonComponent from "@/components/ButtonComponent.vue";
-import SearchbarComponent from "@/components/SearchbarComponent";
+import SearchbarComponent from "@/components/SearchbarComponent.vue";
 const app = createApp(App).use(router);
 app.component("ListComponent", ListComponent);
 app.component("HeaderComponent", HeaderComponent);
