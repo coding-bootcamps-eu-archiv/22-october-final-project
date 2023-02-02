efc9bc48cc8439c936d7ed02af2793319bceed50
Thomas Salmann <thomas.salmann@gmail.com>
2023-02-02 14:46:12 +0100

    add: structure for edditing of list entry

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -3,11 +3,29 @@
     <li v-for="entries in apiState" :key="entries.id">
       <div v-html="entries.title"></div>
       <div v-html="entries.description"></div>
-      <ButtonComponent buttonText="Edit" />
+      <ButtonComponent buttonText="Edit" @click="editEntry(entries.id)" />
       <ButtonComponent
         @click="deleteListElement(entries.id)"
         buttonText="Delete"
       />
+      <!-- <EditFormComponent /> -->
+      <article>
+        <form @submit.prevent>
+          <h2>Title</h2>
+          <input type="text" v-model="title" />
+          <h2>Description</h2>
+          <TiptapComponent v-editor.content="description" />
+          <router-link to="/"
+            ><ButtonComponent
+              buttonText="Submit"
+              @click="putDictionaryEntry(entries.id)"
+              :disabled="isDisabled"
+          /></router-link>
+          <router-link to="/"
+            ><ButtonComponent buttonText="Cancel"
+          /></router-link>
+        </form>
+      </article>
     </li>
   </ul>
 </template>
@@ -18,6 +36,10 @@ export default {
     return {
       apiState: [],
       currentId: undefined,
+      arrayForEditView: [],
+      title: "",
+      description: "",
+      entries: [],
     };
   },
   async mounted() {
@@ -46,10 +68,44 @@ export default {
         }
       }
     },
+    editEntry(id) {
+      // Edit Input muss erscheinen (display none wird inaktiv)
+      // title = title einfügen + description einfügen
+
+      this.currentId = id;
+      for (let item of this.apiState) {
+        if (item.id === id) {
+          console.log(item.title, this.title);
+          this.title = item.title;
+          this.description = item.description;
+        }
+      }
+    },
+    async putDictionaryEntry(id) {
+      console.log(id, this.title);
+      const updatedEntry = {
+        title: this.title,
+        description: this.description,
+        active: false,
+      };
+      await fetch(`http://localhost:3000/entries/${id}`, {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(updatedEntry),
+      });
+      const res = await fetch("http://localhost:3000/entries");
+      const jsonData = await res.json();
+      return (this.entries = jsonData);
+      // this.title = "";
+      // this.description = "";
+    },
   },
   computed: {
     currentUserId() {
       return this.apiState.find((element) => element.id === this.currentId);
+    },
+    isDisabled() {
+      return this.title.length === 0; //|| this.description.length === 0;
     },
   },
 };
---
+++ src/views/EditView.vue
@@ -0,0 +1,6 @@
+<template>
+  <h2>Edit your dictionary entries</h2>
+  <EditFormComponent />
+</template>
+
+<script></script>
