597ca21c99f43a86a49818d07a57ef96d571675e
Chris Oprisiu <chris.oprisiu@gmail.com>
2023-02-04 16:46:49 +0100

    style:searchbar styling

--- src/components/SearchbarComponent.vue
+++ src/components/SearchbarComponent.vue
@@ -1,7 +1,34 @@
 <template>
   <form @submit.prevent>
-    <input type="text" placeholder="Suchwort" />
+    <input type="text" placeholder="Suchwort" id="search-input" />
     <ButtonComponent buttonText="Search" />
+    <div id="lupe">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        fill="#6A1CC3"
+        class="bi bi-search"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+        />
+      </svg>
+    </div>
   </form>
 </template>
-<style scoped></style>
+<style scoped>
+#search-input {
+  padding: 20px;
+  width: 328px;
+  height: 10px;
+  border: 2px solid #6a1cc3;
+  border-radius: 0px;
+}
+#lupe {
+  margin-left: 350px;
+  margin-right: 0rem;
+  fill: #6a1cc3;
+}
+</style>
--- src/views/AttendeeView.vue
+++ src/views/AttendeeView.vue
@@ -1,8 +1,34 @@
 <template>
   <form @submit.prevent>
-    <input type="text" placeholder="Suchwort" v-model="searchText" />
-    <ButtonComponent buttonText="Search" @click="searchTitle(searchText)" />
-    <ButtonComponent buttonText="Show All " @click="showAllEntries" />
+    <div class="container">
+      <input
+        type="text"
+        name="search-input"
+        id="search-input"
+        placeholder="Suchwort"
+        v-model="searchText"
+      />
+      <label for="search-input">
+        <div id="lupe">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="#6A1CC3"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+            />
+          </svg>
+        </div>
+      </label>
+    </div>
+    <div class="button-position">
+      <ButtonComponent buttonText="Search" @click="searchTitle(searchText)" />
+      <ButtonComponent buttonText="Show All " @click="showAllEntries" />
+    </div>
   </form>
   <!-- <ListComponent /> -->
   <AttendeeViewList v-if="searchResults.length === 0" />
@@ -74,3 +100,35 @@ export default {
   },
 };
 </script>
+<style scoped>
+.container {
+  display: inline-block;
+  position: relative;
+  width: 100%;
+}
+#search-input {
+  padding: 10px;
+  height: 10px;
+  border-radius: 0px;
+  border: 3px solid #6a1cc3;
+}
+#lupe {
+  position: absolute;
+  top: 10px;
+  right: 0px;
+  bottom: 0px;
+  margin-right: 10px;
+  fill: #6a1cc3;
+}
+input {
+  width: 100%;
+}
+form {
+  margin: 100px 64px 124px 64px;
+}
+.button-position {
+  display: flex;
+  justify-content: center;
+  margin-top: 12px;
+}
+</style>
